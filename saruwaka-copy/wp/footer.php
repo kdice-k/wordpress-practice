25cbe28a9f3201db87a3551f24e8ef698de704c0
d4isk <dicek@tmick.net>
2020-04-16 02:50:47 +0900

    edit footer
    フッターを修正した

--- saruwaka-copy/wp/footer.php
+++ saruwaka-copy/wp/footer.php
@@ -1,5 +1,11 @@
   <!-- フッター -->
-  <footer><p>&copy; 2020 tmick.net</p></footer>
+  <footer>
+    <div id="tohome"><a href="http://localhost/testWp/">トップページへ</a></div>
+    <div id="copyright">
+      <p class="terms"><a href="#">利用規約</a></p>
+      <p class="copy">&copy; 2020 tmick.net</p>
+    </div>
+  </footer>
   <?php wp_footer(); ?>
 </body>
 </html>
